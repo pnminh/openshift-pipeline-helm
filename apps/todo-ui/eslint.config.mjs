3dbef6c8ab8fd0d8ec7573598991e8962f704f5b
Minh Pham <pnminh232@gmail.com>
2024-05-22 13:52:03 -0500

    app

---
+++ apps/todo-ui/eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
+
+export default [
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReactConfig,
+  {rules: {
+    "react/react-in-jsx-scope": "off"
+  }},
+  {
+    ignores: ["build/*"]
+  },
+  {
+    settings: {
+      "react": {
+        "version": "detect"
+      }
+    }
+  },
+];
---
+++ apps/todo-ui/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    // Indicates which environment to use for testing
+    testEnvironment: 'jsdom',
+    // The glob patterns Jest uses to detect test files
+    testMatch: ['<rootDir>/src/**/*.test.js'],
+    // Transform files before running tests
+    transform: {
+      '^.+\\.jsx?$': 'babel-jest',
+    },
+    // Directories that Jest should use to search for files
+    roots: ['<rootDir>/src'],
+    // Setup files before Jest runs tests
+    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  };
